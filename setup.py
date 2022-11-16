73b13f09060d297429ff4e0762987f53b1c3b4f9
Razvan Gorgan <razvan.gorgan@sectorlabs.ro>
2022-11-16 17:24:11 +0200

    Add black and flake8 command in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,72 @@
 from setuptools import setup, find_packages
+from distutils import cmd, log
+import subprocess
+
+class FormatCommand(cmd.Command):
+    """A custom command to run Pylint on all Python source files."""
+
+    description = 'run black on Python source files'
+    user_options = [
+    ]
+
+    def initialize_options(self):
+        """Set default values for options."""
+        pass
+
+    def finalize_options(self):
+        """Post-process options."""
+        pass
+
+    def run(self):
+        """Run command."""
+        command = ['black', 'src', 'tests']
+        self.announce(
+            'Running command: %s' % str(command),
+            level=log.INFO)
+        subprocess.check_call(command)
+
+class LintCommand(cmd.Command):
+    """A custom command to run Pylint on all Python source files."""
+
+    description = 'run flake8 on Python source files'
+    user_options = [
+    ]
+
+    def initialize_options(self):
+        """Set default values for options."""
+        pass
+
+    def finalize_options(self):
+        """Post-process options."""
+        pass
+
+    def run(self):
+        """Run command."""
+        command = ['flake8', 'src', 'tests']
+        self.announce(
+            'Running command: %s' % str(command),
+            level=log.INFO)
+        subprocess.check_call(command)
+
+class FixCommand(cmd.Command):
+    """A custom command to run Pylint on all Python source files."""
+
+    description = 'run all checks'
+    user_options = [
+    ]
+
+    def initialize_options(self):
+        """Set default values for options."""
+        pass
+
+    def finalize_options(self):
+        """Post-process options."""
+        pass
+
+    def run(self):
+        """Run command."""
+        self.run_command('lint')
+        self.run_command('format')
 
 setup(
     name='watermarker',
@@ -8,4 +76,9 @@ setup(
     install_requires=[line.rstrip('\n') for line in open('requirements.txt')],
     include_package_data=True,
     zip_safe=False,
+    cmdclass={
+        'lint': LintCommand,
+        'format': FormatCommand,
+        'fix': FixCommand
+    },
 )
