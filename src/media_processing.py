684d9e10c92ffa9116e854fe21c5ec396830580b
Razvan Gorgan <razvan.gorgan@sectorlabs.ro>
2022-11-16 15:39:05 +0200

    Fix lint raised errors

--- src/media_processing.py
+++ src/media_processing.py
@@ -1,5 +1,7 @@
 from PIL import Image
 import ffmpeg
+from filetype import filetype
+
 from src.constants import constants
 
 
@@ -32,23 +34,50 @@ def get_overlay(position, margin_nord, margin_south, margin_east, margin_west):
         return f"{get_sign(nord, False)}{get_sign(south, True)}"
 
     if position == 'NE':
-        overlay = f"0{get_horizontal_margins(margin_east, margin_west)}:0{get_vertical_margins(margin_nord, margin_south)}"
+        overlay = (
+            f"0{get_horizontal_margins(margin_east, margin_west)}"
+            f":0{get_vertical_margins(margin_nord, margin_south)}"
+        )
     elif position == 'NC':
-        overlay = f"(main_w/2-overlay_w/2{get_horizontal_margins(margin_east, margin_west)}:0{get_vertical_margins(margin_nord, margin_south)}"
+        overlay = (
+            f"(main_w/2-overlay_w/2{get_horizontal_margins(margin_east, margin_west)}"
+            f":0{get_vertical_margins(margin_nord, margin_south)}"
+        )
     elif position == 'NW':
-        overlay = f"main_w-overlay_w{get_horizontal_margins(margin_east, margin_west)}:0{get_vertical_margins(margin_nord, margin_south)}"
+        overlay = (
+            f"main_w-overlay_w{get_horizontal_margins(margin_east, margin_west)}"
+            f":0{get_vertical_margins(margin_nord, margin_south)}"
+        )
     elif position == 'SE':
-        overlay = f"0{get_horizontal_margins(margin_east, margin_west)}:main_h-overlay_h{get_vertical_margins(margin_nord, margin_south)}"
+        overlay = (
+            f"0{get_horizontal_margins(margin_east, margin_west)}"
+            f":main_h-overlay_h{get_vertical_margins(margin_nord, margin_south)}"
+        )
     elif position == 'SC':
-        overlay = f"(main_w/2-overlay_w/2{get_horizontal_margins(margin_east, margin_west)}:main_h-overlay_h{get_vertical_margins(margin_nord, margin_south)}"
+        overlay = (
+            f"(main_w/2-overlay_w/2{get_horizontal_margins(margin_east, margin_west)}"
+            f":main_h-overlay_h{get_vertical_margins(margin_nord, margin_south)}"
+        )
     elif position == 'SW':
-        overlay = f"main_w-overlay_w{get_horizontal_margins(margin_east, margin_west)}:main_h-overlay_h{get_vertical_margins(margin_nord, margin_south)}"
+        overlay = (
+            f"main_w-overlay_w{get_horizontal_margins(margin_east, margin_west)}"
+            f":main_h-overlay_h{get_vertical_margins(margin_nord, margin_south)}"
+        )
     elif position == 'C':
-        overlay = f"(main_w/2-overlay_w/2{get_horizontal_margins(margin_east, margin_west)}:(main_h/2-overlay_h/2{get_vertical_margins(margin_nord, margin_south)})"
+        overlay = (
+            f"(main_w/2-overlay_w/2{get_horizontal_margins(margin_east, margin_west)}"
+            f":(main_h/2-overlay_h/2{get_vertical_margins(margin_nord, margin_south)})"
+        )
     elif position == 'CE':
-        overlay = f"0{get_horizontal_margins(margin_east, margin_west)}:(main_h/2-overlay_h/2{get_vertical_margins(margin_nord, margin_south)})"
+        overlay = (
+            f"0{get_horizontal_margins(margin_east, margin_west)}"
+            f":(main_h/2-overlay_h/2{get_vertical_margins(margin_nord, margin_south)})"
+        )
     elif position == 'CW':
-        overlay = f"main_w-overlay_w{get_horizontal_margins(margin_east, margin_west)}:(main_h/2-overlay_h/2{get_vertical_margins(margin_nord, margin_south)})"
+        overlay = (
+            f"main_w-overlay_w{get_horizontal_margins(margin_east, margin_west)}"
+            f":(main_h/2-overlay_h/2{get_vertical_margins(margin_nord, margin_south)})"
+        )
     else:
         print(f"Invalid watermark position: {position}")
 
@@ -69,7 +98,7 @@ def get_video_orientation(path):
     # ffprobe needs to be installed as a package on the client OS
     metadata = ffmpeg.probe(path)
     try:
-        rotation = metadata['streams'][0]['tags']['rotate']
+        rotation = metadata['streams'][0]['tags']['rotate']  # noqa: F841
         # TODO - find video with rotate data
     except IndexError:
         # TODO - decide on logging format
@@ -82,7 +111,10 @@ def get_video_orientation(path):
 
 
 def get_watermarking_command(input_file_path, watermark_path, output_file_path, transpose, overlay, watermark_scaling):
-    return f'ffmpeg -y -i {input_file_path} -i {watermark_path} -filter_complex "{transpose}{watermark_scaling}{overlay}" "{output_file_path}"'
+    return (
+        f'ffmpeg -y -i {input_file_path} -i {watermark_path} '
+        f'-filter_complex "{transpose}{watermark_scaling}{overlay}" "{output_file_path}"'
+    )
 
 
 def get_watermark_scaling(path, orientation, watermark_image_ratio):
@@ -126,14 +158,14 @@ def get_media_file_size(file_path):
     # modularize metadata extraction
     try:
         if not metadata or not metadata['streams'][0]['height'] or not metadata['streams'][0]['width']:
-            print(f"No metadata found in file: [{path}]")
+            print(f"No metadata found in file: [{file_path}]")
             return
     except IndexError:
         # TODO - decide on logging format
-        print(f"No video stream found in file: [{path}]")
+        print(f"No video stream found in file: [{file_path}]")
         return
     except KeyError:
-        print(f"No rotation metadata found in file: [{path}]")
+        print(f"No rotation metadata found in file: [{file_path}]")
         return
 
     width = metadata['streams'][0]['width']
@@ -143,3 +175,16 @@ def get_media_file_size(file_path):
         'width': width,
         'height': height
     }
+
+
+def valid_media_file(path):
+    kind = filetype.guess(path)
+    if kind is None:
+        print(f"Cannot guess file type for: {path}")
+        return False
+
+    if not kind.mime.startswith('image') and not kind.mime.startswith('video'):
+        print(f'Invalid media file: [{path}]. Mime: [{kind.mime}]')
+        return False
+
+    return True
